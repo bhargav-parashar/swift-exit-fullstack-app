b1d049605a9e9f0276a1d1c295c5895cd13dd8d8
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-02-24 00:03:43 +0530

    fix: logged in user persist issue

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -78,8 +78,8 @@ const logout = (req,res)=>{
 
 const loginstatus = async (req,res) =>{
     try{
-        const {roleId} = await UserServiceInstance.getUserRoleMapping(reqUser._id);
-        const {role} = await UserServiceInstance.getRole(roleId);
+         const {roleId} = await UserServiceInstance.getUserRoleMapping(req.user._id);
+         const {role} = await UserServiceInstance.getRole(roleId);
 
         res.status(200).json({
             status : true,
